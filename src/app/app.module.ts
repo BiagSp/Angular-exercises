b18bf2be837c4aa08643c26d3846c62d845dc4b7
Biagio Spada <spa.biagio981@gmail.com>
2022-05-05 17:52:55 +0200

    esercizio1 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './container-2/order-list/order-list.component';
+import { OrderSingleComponent } from './container-2/order-single/order-single.component';
+import { OrderDetailComponent } from './container-2/order-detail/order-detail.component';
+import { OrderContainerComponent } from './container-2/order-container/order-container.component';
+import { UserSingleComponent } from './container-principale/user-single/user-single.component';
+import { UserListComponent } from './container-principale/user-container/user-list/user-list.component';
+import { UserContainerComponent } from './container-principale/user-container/user-container.component';
+import { UserDetailComponent } from './container-principale/user-container/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserSingleComponent,
+    OrderContainerComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
